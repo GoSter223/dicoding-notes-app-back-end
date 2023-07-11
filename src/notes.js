4aabb81749210912b4594e53b17b46f1096f117b
GoSter223 <alifakbarhilmyauliya2003@gmail.com>
2023-07-11 19:41:11 +0700

    menambahkan project

---
+++ src/notes.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable import/no-extraneous-dependencies */
+const notes = [];
+
+module.exports = notes;
---
+++ src/server.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable import/no-extraneous-dependencies */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+
+init();
